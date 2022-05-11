0d5ee370795d7b036731b011236fa6c2ddea0330
Leon Kan <leonykan@uw.edu>
2022-05-10 17:49:05 -0700

    set secure to true

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(sessions({
     secret: secret,
     saveUninitialized: true,
     cookie: {
-        // secure: true,
+        secure: true,
         // httpOnly: true,
         maxAge: oneDay
     },
